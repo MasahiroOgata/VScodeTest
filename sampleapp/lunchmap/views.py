7312e2b285af3c49f5b362f911383ffb9486de87
MasahiroOgata <ogata0801@gmail.com>
2025-05-27 22:32:28 +0900

    templateの作成

--- sampleapp/lunchmap/views.py
+++ sampleapp/lunchmap/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views import generic
+from .models import Category, Shop
 
-# Create your views here.
+class IndexView(generic.ListView):
+    model = Shop
