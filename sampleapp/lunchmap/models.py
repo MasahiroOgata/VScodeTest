fb5d6575e2721b725fd74dbc5e3e79aa24f88d4a
MasahiroOgata <ogata0801@gmail.com>
2025-05-28 15:39:43 +0900

    投稿・編集機能の作成

--- sampleapp/lunchmap/models.py
+++ sampleapp/lunchmap/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -13,20 +14,25 @@ class Category(models.Model):
         return self.name
     
 class Shop(models.Model):
-    name = models.CharField(max_length=255)
-    address = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name='店名')
+    address = models.CharField(max_length=255, verbose_name='住所')
     author = models.ForeignKey(
         'auth.User',
         on_delete=models.CASCADE,
+        verbose_name='投稿者',
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.PROTECT,
+        verbose_name='カテゴリ',
     )
-    comment = models.TextField(max_length=1000)
+    comment = models.TextField(max_length=1000, verbose_name='コメント')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('lunchmap:detail', kwargs={'pk':self.pk})
 
